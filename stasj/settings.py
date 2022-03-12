a6794ededebc591e442e1254f3ca14a56085a039
realgudv1n <20vs.stashkevich.a@pdu.by>
2022-03-12 12:21:32 +0300

    JWT Auth

--- stasj/settings.py
+++ stasj/settings.py
@@ -132,11 +132,17 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
-# STATIC_ROOT = '/code/static/'
 STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+}
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
--- user/urls.py
+++ user/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from rest_framework_simplejwt import views as jwt_views
 
 from .views import UserCreate, UserList, UserRetrieve
 
@@ -6,4 +7,6 @@ urlpatterns = [
     path('', UserList.as_view()),
     path('register', UserCreate.as_view()),
     path('<int:pk>', UserRetrieve.as_view()),
+    path('login', jwt_views.TokenObtainPairView.as_view()),
+    path('login/refresh', jwt_views.TokenRefreshView.as_view()),
 ]
